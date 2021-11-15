598caaa9fb2dbed567e139badef47b7b5272a483
sakuniego <egodawatte@wisc.edu>
2021-11-15 15:37:47 -0600

    finished milestone 1

--- app/src/main/java/com/example/a407_lab7_sakuniego/MainActivity.java
+++ app/src/main/java/com/example/a407_lab7_sakuniego/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.a407_lab7_sakuniego;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import static com.example.a407_lab7_sakuniego.App.CHANNEL_1_ID;
+import static com.example.a407_lab7_sakuniego.App.CHANNEL_2_ID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NotificationManagerCompat notificationManager;
+    private EditText editTitle;
+    private EditText editMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTitle = (EditText) findViewById(R.id.editTextTitle);
+        editMessage = (EditText) findViewById(R.id.editTextMessage);
+
+        notificationManager = NotificationManagerCompat.from(this);
+    }
+
+    public void sendOnChannel1(View v) {
+        String title = editTitle.getText().toString();
+        String message = editMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_chat_black_24dp)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManager.notify(1, notification);
+    }
+
+    public void sendOnChannel2(View v) {
+        String title = editTitle.getText().toString();
+        String message = editMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_chat2_black_24dp)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManager.notify(2, notification);
     }
 }
